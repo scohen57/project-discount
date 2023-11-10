43ed8d753991c5724e023c8cf4c46470c6146481
Sam <scohencs@gmail.com>
2023-11-10 13:01:53 -0600

    added in basic tasklist functionality to learn more react

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import InputField from './components/inputField';
+import TodoList from './components/todoList';
+import { Todo } from './todoModel';
 
 const App: React.FC = () => {
+
+  const[todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if(todo) {
+      setTodos([...todos, {id:Date.now(), todo: todo, isDone: false}])
+      setTodo("");
+    }
+  };
+
+  console.log(todos);
+
   return (
     <div className="App">
       <span className='heading'>Taskify</span>
-      <InputField />
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+      <TodoList todos={todos} setTodos={setTodos} />
     </div>
   );
 }
--- src/components/inputField.tsx
+++ src/components/inputField.tsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import "./styles.css";
-const InputField = () => {
-  return ( 
-  <form className='input'>
-    <input type='input' placeholder='Enter a task' className='input_box' />
+
+interface Props{
+    todo: string;
+    setTodo: React.Dispatch<React.SetStateAction<string>>;
+    handleAdd: (e: React.FormEvent) => void;
+}
+
+const InputField = ({todo, setTodo, handleAdd}: Props) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+  
+    return ( 
+  <form className='input' onSubmit={(e) => {
+    handleAdd(e);
+    inputRef.current?.blur();
+    }}>
+    <input 
+        ref={inputRef}
+        type='input' 
+        value={todo} 
+        onChange={
+            (e) => setTodo(e.target.value)
+        } 
+        placeholder='Enter a task' 
+        className='input_box' 
+    />
     <button className='input_submit' type='submit'> Go </button>
   </form>
   )
